21245c95f5981278edb56ea88b0faa205bbd61b3
baijl <bjlmx1314>
2020-07-29 16:57:01 +0800

    dev分支提交

--- src/main/java/com/data/easystudy/EasystudyApplication.java
+++ src/main/java/com/data/easystudy/EasystudyApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * dev分支
+ */
 @SpringBootApplication
 @EnableAsync
 @MapperScan("com.data.easystudy.mapper")
